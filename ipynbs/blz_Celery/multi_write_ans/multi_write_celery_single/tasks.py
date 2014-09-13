08b8d871a306feca4d6e52ba5d3df41d8b560ff6
dboyliao <qmalliao@gmail.com>
2014-09-14 00:46:42 +0800

    update dboy's slides.

--- ipynbs/blz_Celery/multi_write_ans/multi_write_celery_single/tasks.py
+++ ipynbs/blz_Celery/multi_write_ans/multi_write_celery_single/tasks.py
@@ -2,10 +2,10 @@ from __future__ import absolute_import
 from celery import Celery
 
 app = Celery(
-	'multi_write_celery',
+	'multi_write_celery_single',
     broker='mongodb://220.133.188.246',
     backend='mongodb://220.133.188.246',
-    include=['multi_write_celery.tasks']
+    include=['tasks']
 )
 
 # Optional configuration, see the application user guide.
@@ -16,3 +16,7 @@ app.conf.update(
 if __name__ == '__main__':
     app.start()
 
+@app.task
+def multi_write(path, content):
+    with open(path, "w") as wf:
+        wf.write(content)
